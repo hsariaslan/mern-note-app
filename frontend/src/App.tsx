1cb8f00de529932de88359b59648a009e1df22a7
hsariaslan <hakan.sariaslan.job@gmail.com>
2024-07-06 10:24:35 +0300

    Fetch notes from backend.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import { Note } from "./models/note";
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch('/api/v1/notes', { method: 'GET' });
+        const data = await response.json();
+        setNotes(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadNotes();
+  }, []);
+
   return (
-    <div className="app">Hello World!</div>
+    <div className="app">
+      { JSON.stringify(notes) }
+    </div>
   );
 }
 
