3d9a08c220d77f104280d9a7f176812848c8f0c5
hakan <hakan.sariaslan.job@gmail.com>
2024-08-07 21:22:11 +0300

    Authentication logic has been written.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,29 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
 import notesRoutes from "./routes/notes";
+import usersRoutes from "./routes/users";
+import session from "express-session";
+import env from "./util/validateEnv"
+import MongoStore from "connect-mongo"
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_CONNECTION
+    })
+}));
+
 app.use("/api/v1/notes", notesRoutes);
+app.use("/api/v1/users", usersRoutes);
 app.use((req, res, next) => {
     next(createHttpError(404, "Endpoint not found!"));
 });
