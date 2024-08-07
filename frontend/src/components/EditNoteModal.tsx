8fc187fb369ac19fed9e162f2544ced1e07d637a
hakan <hakan.sariaslan.job@gmail.com>
2024-08-08 00:20:09 +0300

    Create and implement TextInputField component

--- frontend/src/components/EditNoteModal.tsx
+++ frontend/src/components/EditNoteModal.tsx
@@ -2,6 +2,7 @@ import {Button, Form, Modal} from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
 import {useForm} from "react-hook-form";
 import * as NotesApi from "../network/notes_api";
+import TextInputField from "./form/TextInputField";
 
 interface EditNoteModalProps {
     note?: NoteModel,
@@ -48,29 +49,21 @@ const EditNoteModal = ({note, onDismiss, onNoteSaved}: EditNoteModalProps) => {
             </Modal.Header>
             <Modal.Body>
                 <Form id="addNoteForm" onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>
-                            Title
-                        </Form.Label>
-                        <Form.Control
-                            type="text"
-                            isInvalid={!!errors.title}
-                            {...register("title", {required: "Required"})}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.title?.message}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>
-                            Text
-                        </Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows={5}
-                            {...register("text")}
-                        />
-                    </Form.Group>
+                    <TextInputField
+                        name="title"
+                        label="Title"
+                        register={register}
+                        registerOptions={{required: "Required"}}
+                        error={errors.title}
+                        type="text"
+                    />
+                    <TextInputField
+                        name="text"
+                        label="Text"
+                        register={register}
+                        as="textarea"
+                        rows="4"
+                    />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
