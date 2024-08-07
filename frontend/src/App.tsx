02c3862f343df83948750a6d78f273ef41b19050
hakan <hakan.sariaslan.job@gmail.com>
2024-08-07 19:59:01 +0300

    Check loading, error and empty states then display corresponding message.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Note as NoteModel} from "./models/note";
 import Note from "./components/Note";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row, Spinner} from "react-bootstrap";
 import styles from "./styles/NotesPage.module.css";
 import * as NotesApi from "./network/notes_api";
 import AddNoteModal from "./components/AddNoteModal";
@@ -10,15 +10,22 @@ import EditNoteModal from "./components/EditNoteModal";
 function App() {
     const [notes, setNotes] = useState<NoteModel[]>([]);
     const [showAddNoteModal, setShowAddNoteModal] = useState(false);
-    const [editingNote, setEditingNote] = useState<NoteModel|null>(null);
+    const [editingNote, setEditingNote] = useState<NoteModel | null>(null);
+    const [notesLoading, setNotesLoading] = useState(true);
+    const [showNotesLoadingError, setShowNotesLoadingError] = useState(false);
 
     useEffect((): void => {
         async function loadNotes(): Promise<void> {
             try {
+                setNotesLoading(true);
+                setShowNotesLoadingError(false);
                 const data: NoteModel[] = await NotesApi.fetchNotes();
                 setNotes(data);
             } catch (error) {
                 console.log(error);
+                setShowNotesLoadingError(true);
+            } finally {
+                setNotesLoading(false);
             }
         }
 
@@ -34,23 +41,35 @@ function App() {
         }
     }
 
+    let notesGrid =
+        <Row xs={1} md={2} xl={3} className="g-4 w-100">
+            {notes.map((note: NoteModel) => (
+                <Col key={note._id}>
+                    <Note
+                        note={note}
+                        onNoteClicked={setEditingNote}
+                        onDeleteClicked={deleteNote}
+                        className={styles.note}
+                    />
+                </Col>
+            ))}
+        </Row>
+
     return (
-        <Container>
+        <Container className="d-flex flex-column align-items-center">
             <Button onClick={() => setShowAddNoteModal(true)} className="my-4 mx-auto d-block">
                 Add Note
             </Button>
-            <Row xs={1} md={2} xl={3} className="g-4">
-                {notes.map((note: NoteModel) => (
-                    <Col key={note._id}>
-                        <Note
-                            note={note}
-                            onNoteClicked={setEditingNote}
-                            onDeleteClicked={deleteNote}
-                            className={styles.note}
-                        />
-                    </Col>
-                ))}
-            </Row>
+            {notesLoading && <Spinner animation="border" variant="primary" />}
+            {showNotesLoadingError && <p>Something went wrong. Re fresh the page.</p>}
+            {!notesLoading && !showNotesLoadingError &&
+                <>
+                    {notes.length > 0
+                    ? notesGrid
+                    : <p>There is not a note! What about creating new 1?</p>
+                    }
+                </>
+            }
             {showAddNoteModal && (
                 <AddNoteModal
                     onDismiss={() => setShowAddNoteModal(false)}
